39f163335db9c64c1a9d034aa032e87c9dc1ea2a
MammadHamidli04 <mhemidli39@gmail.com>
2024-08-28 16:55:25 +0400

    ev isi 1 ve 2 elave edildi

---
+++ DevelopiaProjects/src/homework/SecondHw.java
@@ -0,0 +1,18 @@
+package homework;
+
+public class SecondHw {
+
+	public static void main(String[] args) {
+		int age = 25;
+		System.out.println("age: " + age);
+		boolean hasHome = false;
+		System.out.println("hasHome: " + hasHome);
+		char studentGrade = 'C';
+		System.out.println("studentGrade: " + studentGrade);
+		double carPrice = 25000;
+		System.out.println("carPrice: " + carPrice);
+		String employeeName = "Melik";
+		System.out.println("employeeName: " + employeeName);
+	}
+
+}
